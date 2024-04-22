44dd1169c3c4386152ceba9ed00cda7e2a0884ff
Corvin Schapöhler <corvin.schapoehler@novatec-gmbh.de>
2024-04-22 21:05:47 +0200

    Add first steps to podctl

---
+++ podctl/cmd/init.go
@@ -0,0 +1,38 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// initCmd represents the init command
+var initCmd = &cobra.Command{
+	Use:   "init [commander or worker]",
+	Short: "initialize commander or worker",
+	Long: `init will initialize a new commander or worker cluster with all dependencies.
+It applies all changes to the current active Kubernetes cluster in your kube config.
+
+A commander can also be a worker, keep in mind that you need enough resources for this`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("init called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(initCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// initCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// initCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
