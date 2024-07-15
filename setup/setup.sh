8547c7999942bf2bb5fde072a625eb8d59b80ccc
Corvin Schapöhler <corvin.schapoehler@novatec-gmbh.de>
2024-07-15 14:51:42 +0200

    Add automatic Setup for POD
    This adds automatic Setup for POD instances if Argo is setup (using setup.sh)

---
+++ setup/setup.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+set -e
+
+# Install ArgoCD on current Cluster
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+# Setup this repo as Argo Project which will automatically create the Projects for all other tools
+kubectl apply -f setup.yaml
