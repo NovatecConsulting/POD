4e7ba561d1290983cf9e61ad9cb60177c636d04f
Corvin Schapöhler <corvin.schapoehler@novatec-gmbh.de>
2024-07-22 11:30:53 +0200

    Add EntraID login in ArgoCD
    Add local user for demo showcases
    
    Revert GRPC Route
    
    Add some config for external DNS
    
    Remove namespaces in all manifests
    
    Configure EntraID for SSO
    
    Reconfigure RBAC for Argo
    
    Retry with group instead of mail
    
    Disable argocd admin account, enable anon readonly

--- setup/setup.sh
+++ setup/setup.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Install ArgoCD on current Cluster
-kubectl create namespace argocd
+kubectl create namespace argocd --dry-run=client -o yaml | kubectl apply -f -
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 # Setup this repo as Argo Project which will automatically create the Projects for all other tools
